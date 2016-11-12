19557309634f0f2313d9939d25b2c03ccb2916cd
sunshinelzb <625592890@qq.com>
2016-11-12 23:16:49 +0800

    init stage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,42 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
+//利用自执行函数，将图片信息转成图片URL路径信息
+// var imagesDatas = require('../data/imagesDatas.json');
+// imagesDatas = (function genImageURL(imageDataArr) {
+//     for (var i = 0, len = imageDataArr.length; i < len; i++) {
+//         var singleImageData = imageDataArr[i];
+//         singleImageData.imageURL = require('../images' + singleImageData.fileName);
+//         imageDataArr[i] = singleImageData;
+//     }
+//     return imageDataArr;
+// })(imagesDatas);
 
-class AppComponent extends React.Component {
-  render() {
+// class AppComponent extends React.Component {
+//   render() {
+//     return (
+//       <div className="index">
+//         <img src={yeomanImage} alt="Yeoman Generator" />
+//         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+//         <div>I am a Water boy</div>
+//         <div>webpack</div>
+//       </div>
+//     );
+//   }
+// }
+let AppComponent = React.createClass({
+  render: function () {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec"></section>
+        <nav className="controller-nav"></nav>
+      </section>
     );
   }
-}
-
+})
 AppComponent.defaultProps = {
 };
 
