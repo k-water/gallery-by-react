0ec92ab2523a111b7785d5664b45d19a5ae08610
k-water <625592890@qq.com>
2016-12-14 20:20:39 +0800

    更新图片

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -15,46 +15,42 @@ const dfltPort = 8000;
  * @return {Object}
  */
 function getDefaultModules() {
-  return {
-    preLoaders: [{
-      test: /\.(js|jsx)$/,
-      include: srcPath,
-      loader: 'eslint-loader'
-    }],
-    loaders: [{
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      }, {
-        test: /\.sass/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
-      }, {
-        test: /\.scss/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
-      }, {
-        test: /\.less/,
-        loader: 'style-loader!css-loader!less-loader'
-      }, {
-        test: /\.styl/,
-        loader: 'style-loader!css-loader!stylus-loader'
-      },
-      //{
-      //  test: /\.json$/,
-      //  loader:'json-loader'
-      //},
-      {
-        test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svgs)$/,
-        loader: 'url-loader?limit=8192'
-      }, {
-        test: /\.(mp4|ogg|svg)$/,
-        loader: 'file-loader'
-      }
-    ]
-  };
+    return {
+        preLoaders: [{
+            test: /\.(js|jsx)$/,
+            include: srcPath,
+            loader: 'eslint-loader'
+        }],
+        loaders: [{
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }, {
+                test: /\.sass/,
+                loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+            }, {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+            }, {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            }, {
+                test: /\.styl/,
+                loader: 'style-loader!css-loader!stylus-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svgs)$/,
+                loader: 'url-loader?limit=8192'
+            }, {
+                test: /\.(mp4|ogg|svg)$/,
+                loader: 'file-loader'
+            }
+        ]
+    };
 }
 
 module.exports = {
-  srcPath: srcPath,
-  publicPath: '/assets/',
-  port: dfltPort,
-  getDefaultModules: getDefaultModules
+    srcPath: srcPath,
+    publicPath: '/assets/',
+    port: dfltPort,
+    getDefaultModules: getDefaultModules
 };
